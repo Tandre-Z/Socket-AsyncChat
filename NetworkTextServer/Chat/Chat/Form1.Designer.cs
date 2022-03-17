b6f7706842237299f5a6034a934488e469eeb481
Tandre_Z <101544191+Tandre-Z@users.noreply.github.com>
2022-03-17 11:21:51 +0800

    基于c#.NET Framework的window窗体应用——服务器端
    解决方案中除聊天室的异步服务器外还上传了主线程阻塞连接和异步连接的socket连接模板

---
+++ NetworkTextServer/Chat/Chat/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Chat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.ButtonInit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBoxUsers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ip";
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(139, 23);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(100, 25);
+            this.textBoxIP.TabIndex = 1;
+            this.textBoxIP.Text = "127.0.0.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "端口号";
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(139, 91);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(100, 25);
+            this.textBoxPort.TabIndex = 3;
+            this.textBoxPort.Text = "8888";
+            // 
+            // ButtonInit
+            // 
+            this.ButtonInit.Location = new System.Drawing.Point(31, 149);
+            this.ButtonInit.Name = "ButtonInit";
+            this.ButtonInit.Size = new System.Drawing.Size(116, 23);
+            this.ButtonInit.TabIndex = 4;
+            this.ButtonInit.Text = "启动服务器";
+            this.ButtonInit.UseVisualStyleBackColor = true;
+            this.ButtonInit.Click += new System.EventHandler(this.ButtonInit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 253);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "当前连接用户：";
+            // 
+            // listBoxUsers
+            // 
+            this.listBoxUsers.FormattingEnabled = true;
+            this.listBoxUsers.ItemHeight = 15;
+            this.listBoxUsers.Location = new System.Drawing.Point(34, 290);
+            this.listBoxUsers.Name = "listBoxUsers";
+            this.listBoxUsers.Size = new System.Drawing.Size(205, 244);
+            this.listBoxUsers.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 621);
+            this.Controls.Add(this.listBoxUsers);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ButtonInit);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Button ButtonInit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBoxUsers;
+    }
+}
+
---
+++ NetworkTextServer/Chat/Chat/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chat
+{
+    public partial class Form1 : Form
+    {
+        class ClientInfo
+        {
+            public Socket socket;
+            public byte[] bytes=new byte[1024];
+        }
+
+        SynchronizationContext synchronizationContext;
+
+
+        List<ClientInfo> socketClients = new List<ClientInfo>();
+
+
+        public Form1()
+        {
+            synchronizationContext = SynchronizationContext.Current;
+            InitializeComponent();
+        }
+
+        private void ButtonInit_Click(object sender, EventArgs e)
+        {
+            InitServer();
+        }
+
+        private void InitServer()
+        {
+
+            Socket socketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //服务端需要绑定监听的IP和端口号
+            string ip=textBoxIP.Text;
+            int port = int.Parse(textBoxPort.Text);
+            IPAddress iPAddress = IPAddress.Parse(ip);
+            IPEndPoint iPEndPoint = new IPEndPoint(iPAddress, port);
+
+            socketServer.Bind(iPEndPoint);
+            socketServer.Listen(0);
+            Console.WriteLine("服务器异步启动成功");
+
+            socketServer.BeginAccept(AcceptCallback, socketServer);
+
+        }
+
+        private void AcceptCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket socketServer = (Socket)ar.AsyncState;
+                Socket socketClient = socketServer.EndAccept(ar);
+
+                ClientInfo clientInfo = new ClientInfo();
+                clientInfo.socket = socketClient;
+                socketClients.Add(clientInfo);
+
+                //socketClients.Add(socketClient);
+
+                //显示具体信息到listboxuser
+                synchronizationContext.Post(ShowClientInfo, clientInfo);
+                Console.WriteLine("客户端异步连接服务器成功");
+
+                //等待接收信息
+                clientInfo.socket.BeginReceive(clientInfo.bytes, 0, 1024, 0, ReceiveCallBack, clientInfo);
+
+                //继续等待接受客户端连接
+                socketServer.BeginAccept(AcceptCallback, socketServer);
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine(se.Message);
+                throw;
+            }
+            
+        }
+
+        private void ReceiveCallBack(IAsyncResult ar)
+        {
+            try
+            {
+                ClientInfo clientInfo = (ClientInfo)ar.AsyncState;
+                int count = clientInfo.socket.EndReceive(ar);
+
+                if (count <= 0)
+                {
+                    //线程安全，对象的调用，变量的值
+                    string ip = FormatAddress(clientInfo.socket);
+
+                    //删除list里面的用户信息
+                    socketClients.Remove(clientInfo);
+                    //通知其他客户端某客户端下线
+                    //SendAsync(clientInfo.socket, ip + "已下线");
+                    //关闭该连接
+                    clientInfo.socket.Close();
+                    //删除界面上的UI
+                    synchronizationContext.Post(RemoveClientInfo,ip);
+                    Console.WriteLine("客户端关闭连接");
+                    return;
+                }
+
+                //对接收到的socket信息进行封装
+                string str = Encoding.UTF8.GetString(clientInfo.bytes, 0, count);
+                string[] strs = str.Split('|');
+                string sendStr = FormatAddress(clientInfo.socket) +"|" +strs[1];
+                Console.WriteLine("服务端收到的异步信息是：" + sendStr);
+
+                //遍历客户端并发送
+                for (int i = 0; i < socketClients.Count; i++)
+                {
+                    if(strs[0]=="0")
+                        SendAsync(socketClients[i].socket, sendStr);
+                    else
+                    {
+                        if(FormatAddress(socketClients[i].socket) ==strs[0])
+                        {
+                            SendAsync(socketClients[i].socket,sendStr);
+                        }
+                    }
+                }
+
+                //继续等待接收信息
+                clientInfo.socket.BeginReceive(clientInfo.bytes, 0, 1024, 0, ReceiveCallBack, clientInfo);
+
+            }
+            catch (SocketException se)
+            {
+
+                Console.WriteLine(se.Message);
+            }
+            catch(ArgumentOutOfRangeException aore)
+            {
+                Console.WriteLine(aore.Message);
+            }
+
+        }
+
+        private void SendAsync(Socket socketClient, string value)
+        {
+            byte[] sendBytes = Encoding.UTF8.GetBytes(value);
+            socketClient.BeginSend(sendBytes, 0, sendBytes.Length, 0, SendCallback, socketClient);
+        }
+
+        private void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket socket = (Socket)ar.AsyncState;
+                int count = socket.EndSend(ar);
+                Console.WriteLine("信息发送成功，长度为：" + count);
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine(se.Message);
+            }
+
+        }
+
+        #region 子线程调用主线程UI的回调函数
+        //在服务端主线程UI显示具体信息到listboxuser
+        private void ShowClientInfo(object obj)
+        {
+            ClientInfo clientInfo = (ClientInfo)obj;
+            string ip = FormatAddress(clientInfo.socket);
+            listBoxUsers.Items.Add(ip);
+        }
+
+        //移除服务端主线程UI对应listboxuser的项
+        private void RemoveClientInfo(object obj)
+        {
+            string ip = (string)obj;
+            listBoxUsers.Items.Remove(ip);
+        }
+        #endregion
+
+        //Socket地址格式化
+        private string FormatAddress(Socket socket)
+        {
+            string clientIP = ((IPEndPoint)socket.RemoteEndPoint).Address.ToString();
+            string clientPort = ((IPEndPoint)socket.RemoteEndPoint).Port.ToString();
+
+            return string.Format("{0}:{1}", clientIP, clientPort);
+           
+        }
+
+    }
+}
